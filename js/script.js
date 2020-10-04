3dc6c2326c880b91f6a32ecd47fd4a1504746792
Josh Seltzer <jselt88@gmail.com>
2020-10-04 16:09:30 -0400

    displays a page

--- js/script.js
+++ js/script.js
@@ -1,30 +1,48 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
+const itemsPerPage = 9;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function displayPage(list, page){
+    const startIndex = (page * itemsPerPage) - itemsPerPage;
+    const endIndex = (page * itemsPerPage);
+    const ul = document.querySelector('ul.student-list');
+    ul.innerHTML = '';
 
+    for (let i = startIndex; (i >= startIndex && i < endIndex); i++) {
+        const img = document.createElement('img');
+        img.className = 'avatar';
+        img.src = data[i].picture.medium;
+        img.alt = 'profile picture';
+        const h3 = document.createElement('h3');
+        h3.textContent = `${data[i].name.first} ${data[i].name.last}`;
+        const span = document.createElement('span');
+        span.textContent = data[i].email;
+        const div = document.createElement('div');
+        const li = document.createElement('li');
+        li.className = ('student-item of');
+        const div2 = document.createElement('div');
+        div2.className = ('joined-details');
+        const span2 = document.createElement('span');
+        span2.className = ('date');
+        span2.textContent = `Joined: ${data[i].registered.date}`;
+        div2.appendChild(span2);
+        div.appendChild(img);
+        div.appendChild(h3);
+        div.appendChild(span);
+        li.appendChild(div);
+        li.appendChild(div2);
+        ul.appendChild(li);
+
+    }
+}
 
 
 /*
 Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
 */
 
 
-
 // Call functions
+displayPage(data, 1);
